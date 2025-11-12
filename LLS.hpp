8fec3f0bb8975b3bf685278faca1b0ce750229d7
Lalithaaaa <126942751+Lalithaaaa@users.noreply.github.com>
2025-11-12 02:28:19 +0000

    lls

--- LLS.hpp
+++ LLS.hpp
@@ -11,17 +11,29 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLS();
+    LLS() {
+        list =  LinkedList<T>();
+    }
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override { //add node at head
+        list.addHead(item);
+    }
 
     // Deletion
-    T pop() override;
+    T pop() override { //remove and return head
+        T d = (list.getHead)->data;
+        list.removeHead();
+        return d;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override {
+        return (list.getHead)->data;
+    }
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return list.getCount();
+    }
 };
