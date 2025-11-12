97da471a5798271877fc776e8d18f646b518486a
Lalithaaaa <126942751+Lalithaaaa@users.noreply.github.com>
2025-11-12 03:31:53 +0000

    LLDQ

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,59 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() {
+        list = LinkedList<T>();
+    }
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override {
+        list.addHead(item);
+    }
+    void pushBack(const T& item) override {
+        list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override {
+        if(list.getHead() == nullptr) {
+            throw std::runtime_error ("e or");
+        }
+        
+        T d = (list.getHead())->data;
+        list.removeHead();
+        return d;
+    }
+
+    T popBack() override {
+        if(list.getTail() == nullptr) {
+            throw std::runtime_error ("e or");
+        }
+        
+        T d = (list.getTail())->data;
+        list.removeTail();
+        return d;
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        if(list.getHead() == nullptr) {
+            throw std::runtime_error ("e or");
+        }
+        
+        return (list.getHead())->data;
+    }
+    const T& back() const override {
+        if(list.getTail() == nullptr) {
+            throw std::runtime_error ("e or");
+        }
+        
+        return (list.getTail())->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return list.getCount();
+    }
 };
 
 
